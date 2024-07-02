51f48e430c83f821dd1fbbcbbc940169aefd9d2f
SolanoJason <pajason2000@outlook.com>
2024-07-02 17:52:22 -0500

    feat: Skillset badges

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,50 @@
 module.exports = {
   content: ['./**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        whatsapp: {
+          DEFAULT: '#00d757',
+        },
+        hackerrank: {
+          DEFAULT: '#28b95a',
+        },
+        linkedin: {
+          DEFAULT: '#116bc5',
+        },
+      },
+      animation: {
+        typewriter: 'typewriter 1.3s steps(17) forwards',
+        caret: 'typewriter 1.3s steps(17) forwards, blink 1s steps(17) infinite 1.3s',
+      },
+      keyframes: {
+        typewriter: {
+          to: {
+            left: '100%',
+          },
+        },
+        blink: {
+          '0%': {
+            opacity: '0',
+          },
+          '0.1%': {
+            opacity: '1',
+          },
+          '50%': {
+            opacity: '1',
+          },
+          '50.1%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '0',
+          },
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+
+  ],
 }
 
