5166c22f08e012c5b50f5db36fba28f417507026
SolanoJason <pajason2000@outlook.com>
2024-07-14 00:20:22 -0500

    feat: add language support for english and spanish

--- src/js/script.js
+++ src/js/script.js
@@ -28,3 +28,68 @@ if (!localStorage.getItem('dark-mode')) {
     } else {
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuButton = document.getElementById('menu-button');
+    const dropdownMenu = document.getElementById('dropdown-menu');
+    const englishButton = document.getElementById('menu-item-english');
+    const spanishButton = document.getElementById('menu-item-spanish');
+
+    menuButton.addEventListener('click', function () {
+        const isExpanded = menuButton.getAttribute('aria-expanded') === 'true';
+        menuButton.setAttribute('aria-expanded', !isExpanded);
+        dropdownMenu.classList.toggle('hidden');
+    });
+
+    // Close the dropdown menu if clicking outside of it
+    document.addEventListener('click', function (event) {
+        if (!menuButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+            menuButton.setAttribute('aria-expanded', 'false');
+            dropdownMenu.classList.add('hidden');
+        }
+    });
+
+    menuButton.click();
+
+    let lang = navigator.language || navigator.userLanguage;
+    let enList = document.getElementsByClassName('en');
+    let esList = document.getElementsByClassName('es');
+
+    function toEnglish() {
+        for (const enInstance of enList) {
+            enInstance.classList.remove('hidden');
+        }
+        for (const esInstance of esList) {
+            esInstance.classList.add('hidden');
+        }
+        englishButton.classList.add('btn-active');
+        spanishButton.classList.remove('btn-active');
+    }
+
+    function toSpanish() {
+        for (const enInstance of enList) {
+            enInstance.classList.add('hidden');
+        }
+        for (const esInstance of esList) {
+            esInstance.classList.remove('hidden');
+        }
+        spanishButton.classList.add('btn-active');
+        englishButton.classList.remove('btn-active');
+    }
+
+    if (lang.startsWith('en')){
+        toEnglish();
+    } else if (lang.startsWith('es')) {
+        toSpanish();
+    } else {
+        toEnglish();
+    }
+
+    spanishButton.onclick = (e) => {
+        toSpanish();
+    }
+    englishButton.onclick = (e) => {
+        toEnglish();
+    }
+
+});
